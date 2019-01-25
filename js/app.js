5d7f4bec5028bee8c39edcec0db60613ef68efb7
Evgeni Vershinin <jeve140495@outlook.com>
2019-01-25 13:42:59 +0100

    Remastered

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const game = new Game();
+
+/**
+ * Listens for click on `#begin-game` and calls startGmae() on game object
+ */
+document.getElementById('begin-game').addEventListener('click', function()
+{
+    game.startGame();
+    this.style.display = 'none';
+    document.getElementById('play-area').style.opacity = '1';
+});
+
+/**
+ * Listens for keyboard presses
+ */
+document.addEventListener('keydown', function(event)
+{
+    game.handleKeydown(event);
+});
